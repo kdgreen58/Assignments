5c113121164cd3857af728d33b5598fe5c8cfc26
kdgreen58 <kdgreen58@gmail.com>
2014-09-01 19:15:06 -0400

    Update Assignment1_KoltonGreen.R

--- Assignment1_KoltonGreen.R
+++ Assignment1_KoltonGreen.R
@@ -1,11 +1,11 @@
 > #======================================================================================
 > #Name: Kolton D. Green
-  > #Date: 9/1/2014
-  > #Course: IS 360 02
-  > #Assignment 1
-  > #Summary: Provides answers to three questions written in R Script and uploaded to Github. Questions include those asking for versions of software used and number of observations in a dataset.
-  > #===================================================================================
+> #Date: 9/1/2014
+> #Course: IS 360 02
+> #Assignment 1
+> #Summary: Provides answers to three questions written in R Script and uploaded to Github. Questions include those asking for versions of software used and number of observations in a dataset.
+> #===================================================================================
 > #1. R Version: 3.1.0, RStudio Version: 0.98.1028
-  > #2. PostgreSQL Version: 9.3.5
-  > #3. Observations in sales dataset: 401,146
-  > #End
+> #2. PostgreSQL Version: 9.3.5
+> #3. Observations in sales dataset: 401,146
+> #End
