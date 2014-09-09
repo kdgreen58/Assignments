24b474b2edff7caf990a745f70f4450dbb4c1cc7
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-09 07:08:25 -0700

    Week 2 Assignment

---
+++ Assignment2_KoltonGreen.R
@@ -0,0 +1,15 @@
+#======================================================================================
+#Name: Kolton D. Green
+#Date: 9/9/2014
+#Course: IS 360 02
+#Week 2 Assignment
+#Summary: Provide script for a queue system written in R Script and uploaded to Github.
+#===================================================================================
+#1. queue<- c("James","Mary","Steve","Alex","Patricia")
+#2. queue <- c(queue, "Harold")
+#3. queue<- queue[-1]
+#4. queue<- append(queue, "Pam", after=1)
+#5. queue<- queue[-6]
+#6. queue<- queue[!queue %in% "Alex"]
+#7. which(queue %in% "Patricia")
+#8. length(queue)
