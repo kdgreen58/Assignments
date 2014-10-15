82230e4445fbeb566aa8cda5339c82117176a7c0
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-10-14 23:29:31 -0400

    Week 7 assignment.

---
+++ Week7_KoltonGreen.R
@@ -0,0 +1,25 @@
+#======================================================================================
+#Name: Kolton D. Green
+#Date: 10/14/2014
+#Course: IS 360 02
+#Week 7 Assignment
+#Summary: Provide analysis overview of four data sets.
+#======================================================================================
+# attach(Week7AssignmentDataI)
+# par(mfrow = c(2,2))
+# plot(XI, YI, main = "Data Set I", xlab = "x", ylab = "y")
+# plot(XII, YII, main = "Data Set II", xlab = "x", ylab = "y")
+# plot(XIII, YIII, main = "Data Set III", xlab = "x", ylab = "y")
+# plot(XIV, YIV, main = "Data Set IV", xlab = "x", ylab = "y")
+#
+# This plots four scatterplots all on the same view. 
+#
+# Data set one looks like a completely random data set where if a regression line was 
+# drawn through it not all points will be on the line but no more than one 
+# standard deviation away.
+#
+# Data set two looks like a hyperbolic trend of data points peaking at ~x = 11.
+#
+# Data set three looks like an almost perfect trend like, but with one outlier, ~x= 13.
+#
+# Data set four is just a vertical line on x=8.
