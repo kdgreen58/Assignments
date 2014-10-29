fff3f21cb8fab6e998da3109a9b1b4e81104c836
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-10-28 22:00:47 -0400

    Week 9 Code

---
+++ Week9_KoltonGreen.R
@@ -0,0 +1,15 @@
+#======================================================================================
+#Name: Kolton D. Green
+#Date: 10/28/2014
+#Course: IS 360 02
+#Week 9 assignment
+#Summary: Answers to 8 questions on web scrapping
+#======================================================================================
+#1. Data Frame
+#2. A list
+#3. length(hvalues)
+#4. hvalues <- as.data.frame(readHTMLTable(theURL)[1])
+#5. hvaluestwo <- hvalues[, c("NULL.Last.Name", "NULL.Points")]
+#6. http://www.tutorialspoint.com/html/html_tables.htm
+#7. 11
+#8. Chrome, right click and select "View Page Source"
