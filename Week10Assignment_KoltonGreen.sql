7e68c62b2f37717c94c068ea8fa52b9328810a4f
kdgreen58 <kolton.green@spsmail.cuny.edu>
2014-11-05 19:03:44 -0500

    Week 10 Assignment

---
+++ Week10Assignment_KoltonGreen.sql
@@ -0,0 +1,14 @@
+﻿SELECT country
+FROM country
+INNER JOIN city
+on country.country_id = city.country_id
+WHERE city = 'Abu Dhabi';
+
+SELECT title
+FROM film
+WHERE description LIKE '%Astronaut%';
+
+SELECT title
+FROM film
+WHERE replacement_cost BETWEEN '20.00' AND '24.99' AND title like 'E%'
+ORDER BY title ASC;
